40bd1121fc8d5c54f5fb24f399653acea78ccba4
Aero <Aero>
2017-01-21 12:31:06 +0100

    rebuilded catalog structure

---
+++ src/GraphicsManager.cpp
@@ -0,0 +1,27 @@
+#include "GraphicsManager.h"
+
+GraphicsManager::GraphicsManager()
+{
+    //ctor
+}
+
+GraphicsManager::~GraphicsManager()
+{
+    //dtor
+}
+Texture *GraphicsManager::getTexture(const string &path)
+{
+    return getTexture(path.c_str());
+}
+
+Texture *GraphicsManager::getTexture(const char *path)
+{
+    Texture *texture;
+    texture = 0;
+    /* sprawdza czy jest, jesli nie to dodaje, jesli nie ma takiego pliku zwraca BLANK (wygenerowana) czy cos w tym stylu
+
+
+
+    */
+    return texture;
+}
